322249103235b85bb3f031be603f20397b945721
Juan Jose Prieto Dzul <juanjoseprieto88@gmail.com>
2019-11-12 12:14:45 -0600

    Se agrego bloque de licencia.

--- RemotePrinter/RemotePrinterForm.cs
+++ RemotePrinter/RemotePrinterForm.cs
@@ -1,4 +1,22 @@
-﻿using Microsoft.Win32;
+﻿/*
+ * This file is part of RemotePrinter.
+ * Copyright (C) 2018-2019 Juan José Prieto Dzul <juanjoseprieto88@gmail.com>
+ * 
+ * RemotePrinter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * RemotePrinter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RemotePrinter.  If not, see<https://www.gnu.org/licenses/>.
+ */
+
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
