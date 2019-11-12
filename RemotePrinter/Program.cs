1744b52ac1de87f5d4a355875e62b9106ef2fa46
Juan Jose Prieto Dzul <juanjoseprieto88@gmail.com>
2019-11-12 12:11:21 -0600

    Se eliminaron archivos obsoletos.

--- RemotePrinter/Program.cs
+++ RemotePrinter/Program.cs
@@ -26,9 +26,6 @@ namespace RemotePrinter
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
         [STAThread]
         static void Main()
         {
